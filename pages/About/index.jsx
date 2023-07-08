5b4c655e187dba08d49afb3c81b25d05833123da
ede <mehmet.ede@detaysoft.com>
2023-07-08 03:47:44 +0300

    fix

--- pages/About/index.jsx
+++ pages/About/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full h-screen flex items-center justify-around relative rsm:flex-wrap'>
-      <p className='flex w-fit text-3xl text-white  text-center absolute rsm:mt-96 items-start left-[10rem] -top-10 ' >My Projects </p>
-      <ul className="w-[42%]  inset-0 list-none inline-block rsm:w-full rsm:mt-96  mb-32  text-white ">
+    <div className='w-full h-screen flex items-center justify-around relative rsm:flex-wrap rsm:mt-40'>
+      <p className='flex w-fit text-3xl text-white  text-center absolute rsm:mt-96 items-start left-[10rem] mt-32 -top-10 self-center rsm:left-0 ' >My Projects </p>
+      <ul className="w-[42%]  inset-0 list-none inline-block rsm:w-full rsm:mt-96 mt-10 text-white ">
         <li className='pt-[1px] pb-[1px] p-5 bg-[#0b0b13]'><span className='bg-[#2fbf71] text-white text-xs p-1 align-top tracking-tight -m-1 -left-7 relative mb-[9px] after:absolute
            after:left-[0px]
             after:-bottom-[15px]
@@ -24,7 +24,11 @@ function About() {
              after:border-l-[10px]
               after:border-r-[2px] after:border-t-[15px]
                after:border-t-green-700
-                after:border-r-transparent after:border-l-transparent'>2022-Present </span><h6 className='text-xl mt-3 p-3'>Front-end Developer</h6><p className='p-5' >Bnet Masraf <hr /><br /> Technology: React-Native</p></li>
+                after:border-r-transparent after:border-l-transparent'>2022-Present </span><h6 className='text-xl mt-3 p-3'>Front-end Developer</h6> <span>
+                  <p className='p-5' >Bnet Masraf </p>
+                  <hr /><br />
+                  <p className='p-5'>Technology: React-Native</p>
+                  </span> </li>
         {/* <li className='pt-[1px] pb-[1px] p-5 bg-[#0b0b13]'><span className='bg-[#2fbf71] text-white text-xs p-1 align-top tracking-tight -m-1 -left-7 relative mb-[9px] after:absolute
            after:left-[0px]
             after:-bottom-[15px]
@@ -37,25 +41,25 @@ function About() {
         <p className='text-5xl tracking-wider text-white text-center p-4 underline  inset-0'> My Skills</p>
 
         <label htmlFor="HTML5" className="block mb-2 mt-12 text-white font-bold text-xl">HTML5</label>
-        <input id="JavaScript" type="range" defaultValue="65" className="w-full h-2 rounded-lg appearance-none linear cursor-pointer accent-green-300 bg-gradient-to-r  from-green-700 to-gray-700" />
+        <input id="JavaScript" type="range"  disabled defaultValue="65" className="w-full h-2 rounded-lg appearance-none linear cursor-pointer accent-green-300 bg-gradient-to-r  from-green-700 to-gray-700" />
 
         <label htmlFor="JavaScript" className="block mb-2 mt-3 text-white font-bold text-xl">JavaScript</label>
-        <input id="HTML5" type="range" defaultValue="75" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
+        <input id="HTML5" type="range"  disabled defaultValue="75" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
 
         <label htmlFor="CSS" className="block mb-2 mt-3 text-white font-bold text-xl">CSS</label>
-        <input id="CSS" type="range" defaultValue="55" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
+        <input id="CSS" type="range"  disabled defaultValue="55" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
 
         <label htmlFor="Tailwind" className="block mb-2 mt-3 text-white font-bold text-xl">Tailwind</label>
-        <input id="Tailwind" type="range" defaultValue="45" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
+        <input id="Tailwind" type="range"  disabled defaultValue="45" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
 
         <label htmlFor="ReactJS" className="block mb-2 mt-3 text-white font-bold text-xl">ReactJS</label>
-        <input id="HTML5" type="range" defaultValue="75" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
+        <input id="HTML5" type="range"  disabled defaultValue="75" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
 
         <label htmlFor="NextJS" className="block mb-2 mt-3 text-white font-bold text-xl">NextJS</label>
-        <input id="HTML5" type="range" defaultValue="45" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
+        <input id="HTML5" type="range"  disabled defaultValue="45" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
 
         <label htmlFor="NodeJS" className="block mb-2 mt-3 text-white font-bold text-xl">NodeJS</label>
-        <input id="NodeJS" type="range" defaultValue="55" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
+        <input id="NodeJS" type="range"  disabled defaultValue="55" className="w-full h-2 rounded-lg appearance-none cursor-pointer accent-green-300 bg-gradient-to-r from-green-700 to-gray-700" />
 
       </div>
     </div>
--- pages/MainPage/index.jsx
+++ pages/MainPage/index.jsx
@@ -7,8 +7,7 @@ function MainPage() {
         <div className='w-full h-screen flex items-center justify-evenly relative flex-wrap'>
             <div className='w-[100vh] h-[100vh] bg-[#0b0b13] rounded-full absolute -right-48 -top-60 -z-0'>
             </div>
-
-            <div className='w-[500px] h-[500px] text-white items-center z-10 -mb-48 '>
+            <div className='w-[500px] h-[500px] text-white items-center z-10 -mb-48 rsm:mb-40 '>
                 <p className='text-2xl ' >I am</p>
                 <p className='text-5xl mt-6 mb-6 ' >Mehmet EDE</p>
                 <TypeAnimation sequence={[
@@ -23,9 +22,9 @@ function MainPage() {
                         flexWrap: 'nowrap',
                         color: '#2fbf71'
                     }} />
-                <p className='text-2xl tracking-wider font-mono  mt-6' >{"I'm a front-end developer and I can turn my imagination into reality using JavaScript"}.</p>
+                <p className='text-2xl tracking-wider font-mono  mt-6 ' >{"I'm a front-end developer and I can turn my imagination into reality using JavaScript"}.</p>
             </div>
-            <div className='w-[500px] h-[500px] border-green-100 bg-green-100 bg-opacity-5 backdrop-blur-3xl text-center  drop-shadow-lg border-solid border-2 border-opacity-10 z-10 rsm:z-0 p-10 rounded-xl'>
+            <div className='w-[500px] h-[500px] border-green-100 bg-green-100  rsm:p-2 bg-opacity-5 backdrop-blur-3xl text-center drop-shadow-lg border-solid border-2 border-opacity-10 z-10 rsm:z-0 rsm:mb-40 p-10 rounded-xl'>
                     <p className='text-2xl tracking-wider text-white font-mono  mt-6 pb-6'>
                     {"I'm a Front-end Developer with over 3 years of experience."}
                     </p>
