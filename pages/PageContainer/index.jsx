aa0ce632a82d7c07bfd3085ff4d8745fbc54d8ee
ede <mehmet.ede@detaysoft.com>
2023-07-12 10:39:04 +0300

    Fix PageContainer

--- pages/PageContainer/index.jsx
+++ pages/PageContainer/index.jsx
@@ -1,32 +1,39 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Hamburger from '@/component/Hamburger'
 import MainPage from '../MainPage'
 import About from '../About'
 import SideBar from '@/component/SideBar'
 
 function PageContainer() {
-    const [hide, setHide] = useState(window.innerWidth <= 570 ? true : false)
+    const [hide, setHide] = useState(false)
+  useEffect(() => {
+    var wWidth=window.innerWidth
+    if(wWidth<= 570){
+      setHide(true)
+    }else{
+      setHide(false)
+    }
+  }, [])
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+    <div className={`rsm:${false ? 'hidden' : 'block'} rsm:z-[999] sm:z-[999] md:z-[999] lg:z-[999] sm:block md:block lg:block`}>
+      <SideBar hide={hide} />
+    </div>
 
-    return (
-        <div className='w-full h-screen flex items-center justify-center'>
-            <div className={`rsm:${false ? 'hidden' : 'block'} rsm:z-[999] sm:z-[999] md:z-[999] lg:z-[999] sm:block md:block lg:block`}>
-                <SideBar hide={hide} />
-            </div>
+    <div className="w-full z-50 bg-edeColor p-4 h-screen flex-wrap justify-center overflow-x-hidden rsm:overflow-y-scroll rmd:overflow-y-scroll rlg:overflow-y-scroll">
+      <Hamburger hideValue={setHide} />
+      <section id='homePage' className='scroll-smooth snap-proximity delay-100'>
+        <MainPage />
+      </section>
+      <br />
+      <section id='AboutPage' className='scroll-smooth snap-proximity'>
+        <About />
+      </section>
+    </div>
 
-            <div className="w-full z-50 bg-edeColor p-4 h-screen flex-wrap justify-center overflow-x-hidden rsm:overflow-y-scroll rmd:overflow-y-scroll rlg:overflow-y-scroll">
-                <Hamburger hideValue={setHide} />
-                <section id='homePage' className='scroll-smooth snap-proximity delay-100'>
-                    <MainPage />
-                </section>
-                <br />
-                <section id='AboutPage' className='scroll-smooth snap-proximity'>
-                    <About />
-                </section>
-            </div>
-
-        </div >
-    )
+  </div >
+  )
 }
 
 export default PageContainer
